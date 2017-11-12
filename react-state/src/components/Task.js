0b1524cf78ab0291c481b13774c7c1702f7afe15
willtuck <willtuck@gmail.com>
2017-11-11 16:06:05 -0800

    test

---
+++ react-state/src/components/Task.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class Task extends React.Component {
+
+    render() {
+        let styles = {
+            cursor: "pointer",
+        }
+
+        if (this.props.done) {
+            styles.color = "#AAA";
+            styles.textDecoration = "line-through";
+        }
+
+        return (
+            <li style={styles}
+                onClick= {
+                    () => this.props.toggleDone(this.props.id)
+                 }
+            >
+                {this.props.title}</li>
+        );
+    }
+}
